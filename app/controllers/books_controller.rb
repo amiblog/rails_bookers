2d5b2c6db49e8fbe4d58eb0444818aba01fb4dbe
amiblog <reza.am.0425@gmail.com>
2022-01-11 14:13:18 +0000

    全仕様実装

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all.order(id: :asc)
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+      if @book.save
+        flash[:notice] = "Book was successfully created."
+        redirect_to "/books/#{@book.id}"
+      else
+        @books = Book.all
+        render :index
+      end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to "/books/#{@book.id}"
+    else
+      @books = Book.all
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to "/books"
+  end
+
+end
+
+
+private
+def book_params
+  params.require(:book).permit(:title, :body)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'books' => "books#index"
+  post "books" => "books#create"
+  get 'books/:id' => "books#show"
+  get 'books/:id/edit' => "books#edit"
+  patch "books/:id" => "books#update"
+  delete "books/:id" => "books#destroy"
+  get '/' => "homes#top"
+
 end
